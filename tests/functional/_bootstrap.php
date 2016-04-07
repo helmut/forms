72a2465245e920cd06678fafdc87e6e147970299
helmut <helmut.github@gmail.com>
2016-04-07 10:36:43 +1000

    force timezone on test bootstrap

--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+date_default_timezone_set('UTC');
 
 function script($test) {
 	return str_replace('Cept', '', implode('/', array_slice(explode('/', $test), -2)));
